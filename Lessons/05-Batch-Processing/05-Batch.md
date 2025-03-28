cec7177cd292fd31b4f2c960ee313f666b857029
Henrique Cruzz <henrique.cruzz@gmail.com>
2025-03-27 22:10:12 -0300

    Module 05 - Complete

--- Lessons/05-Batch-Processing/05-Batch.md
+++ Lessons/05-Batch-Processing/05-Batch.md
@@ -22,3 +22,63 @@ Takes some chunks of data, usually Daily data.
 ### When?
 When is in a Datalake (s3/GCS), Spark will put the data, process and put back in the Datalake
 
+### Configuring into Machine:
+(https://github.com/DataTalksClub/data-engineering-zoomcamp/tree/50377191493a29d49f181e4f4081f29655bbd6f3/05-batch/setup)
+export PYTHONPATH="${SPARK_HOME}/python/:$PYTHONPATH"
+export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.10.9.5-src.zip:$PYTHONPATH"
+
+
+
+### Spark Internals
+
+REAL SCENARIO
+
+From your machine you can wrote code in Python and we have a cluster
+Inside this cluster we have a [Master]
+My Machine Submit package to Master.
+A cluster can have multiple machines to Execute the jobs submitted.(Executors)
+
+Machine -> Master -> Executor
+
+### Running locally vs Running With Local Cluster
+
+URL="spark://de-zoomcamp.us-central1-c.c.zoomcamp-2025-451318.internal:7077"
+
+spark-submit \
+    --master="${URL}" \
+    10_SparkSQL_Local.py \
+        --input_green=data/pq/green/2021/*/ \
+        --input_yellow=data/pq/yellow/2021/*/ \
+        --output=data/report/report-2021
+
+
+python 10_SparkSQL_Local.py \
+    --input_green=data/pq/green/2020/*/ \
+    --input_yellow=data/pq/yellow/2020/*/ \
+    --output=data/report/report-2020
+
+### Last Chapter - DataProc
+
+Submiting via Terminal
+
+gcloud dataproc jobs submit pyspark \
+    --cluster=de-zoomcamp-2025-cluster-hc \
+    --region=us-central1 \
+    gs://ny-taxis-bucket-zoomcamp-2025/code/10_sparksql.py \
+    -- \
+        --input_green=gs://ny-taxis-bucket-zoomcamp-2025/pq/green/2021/* \
+        --input_yellow=gs://ny-taxis-bucket-zoomcamp-2025/pq/yellow/2021/* \
+        --output=gs://ny-taxis-bucket-zoomcamp-2025/reports/2021
+
+### Saving in BQ
+
+gcloud dataproc jobs submit pyspark \
+    --cluster=de-zoomcamp-2025-cluster-hc \
+    --region=us-central1 \
+    gs://ny-taxis-bucket-zoomcamp-2025/code/11_sparksql_bq.py \
+    -- \
+        --input_green=gs://ny-taxis-bucket-zoomcamp-2025/pq/green/2020/* \
+        --input_yellow=gs://ny-taxis-bucket-zoomcamp-2025/pq/yellow/2020/* \
+        --output=ny_taxis_zoomcamp_2025.report_2020
+
+
---
+++ Lessons/05-Batch-Processing/05-batch/10_SparkSQL_Local.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+import argparse
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+
+# In[3]:
+
+parser = argparse.ArgumentParser(description="Configs for Spark Job")
+# User, Password, Host, Database Name, Table Name and URL of the csv
+
+parser.add_argument("--input_green", required=True, help="Green Location")
+parser.add_argument("--input_yellow", required=True, help="Yellow Location")
+parser.add_argument("--output", required=True, help="Output Location")
+
+args = parser.parse_args()
+
+input_green = args.input_green
+input_yellow = args.input_yellow
+output = args.output
+
+
+spark = SparkSession.builder \
+    .appName('test') \
+    .getOrCreate()
+
+
+# In[4]:
+
+
+df_green = spark.read.parquet(input_green)
+
+
+# In[5]:
+
+
+#df_green.show(10,False)
+
+
+# In[6]:
+
+
+df_yellow = spark.read.parquet(input_yellow)
+
+
+# In[7]:
+
+
+#df_yellow.show(5,False)
+
+
+# In[8]:
+
+
+df_green = df_green \
+    .withColumnRenamed('lpep_pickup_datetime','pickup_datetime') \
+    .withColumnRenamed('lpep_dropoff_datetime','dropoff_datetime')
+
+
+# In[9]:
+
+
+df_yellow = df_yellow \
+    .withColumnRenamed('tpep_pickup_datetime','pickup_datetime') \
+    .withColumnRenamed('tpep_dropoff_datetime','dropoff_datetime')
+
+
+# In[10]:
+
+
+#Selecting all columns for both
+common_columns = []
+yellow_columns = set(df_yellow.columns)
+for col in df_green.columns:
+    if col in yellow_columns:
+        common_columns.append(col)
+
+
+# In[11]:
+
+
+df_green_sel = df_green.select(common_columns) \
+    .withColumn('service_type', F.lit('green'))
+
+
+# In[12]:
+
+
+df_yellow_sel = df_yellow.select(common_columns) \
+    .withColumn('service_type', F.lit('yellow'))
+
+
+# In[13]:
+
+
+df_trips_data = df_green_sel.unionAll(df_yellow_sel)
+
+
+# In[14]:
+
+
+df_trips_data.groupBy('service_type').count().show()
+
+
+# In[15]:
+
+
+df_trips_data.createOrReplaceTempView('trips_data')
+
+
+# In[16]:
+
+
+#spark.sql("""
+#Select service_type, count(1) as count
+#from trips_data
+#group by service_type
+#""").show()
+
+
+# In[17]:
+
+
+df_result = spark.sql("""
+    select 
+    -- Revenue grouping 
+    PULocationID as revenue_zone,
+    date_trunc('month', cast(pickup_datetime as TIMESTAMP)) as revenue_month,
+    service_type, 
+
+    -- Revenue calculation 
+    sum(fare_amount) as revenue_monthly_fare,
+    sum(extra) as revenue_monthly_extra,
+    sum(mta_tax) as revenue_monthly_mta_tax,
+    sum(tip_amount) as revenue_monthly_tip_amount,
+    sum(tolls_amount) as revenue_monthly_tolls_amount,
+    sum(improvement_surcharge) as revenue_monthly_improvement_surcharge,
+    sum(total_amount) as revenue_monthly_total_amount,
+
+    -- Additional calculations
+    avg(passenger_count) as avg_monthly_passenger_count,
+    avg(trip_distance) as avg_monthly_trip_distance
+
+    from trips_data
+    group by revenue_zone, revenue_month, service_type
+""")
+
+
+# In[18]:
+
+
+#df_result.show(5)
+
+
+# In[56]:
+
+
+df_result.coalesce(1).write.mode('overwrite').parquet(output)
+
+
